661659b0cccf47079c3aa44b50a1e9b4a30f945d
zcemycl <lyc010197@gmail.com>
2024-04-02 17:35:44 +0100

    update

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -2,7 +2,7 @@
 import { useState, useEffect } from "react";
 import { useSearchParams, useRouter } from "next/navigation";
 import Link from "next/link";
-import { useAuth } from "@/contexts/Auth";
+import { useAuth } from "@contexts/Auth";
 // import { useAuth } from "../../../contexts/Auth";
 
 type IMode = "login" | "verify";
