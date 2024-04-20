13e2fa36798d2d96ce12cc55ae5b4176fa6a9965
zcemycl <lyc010197@gmail.com>
2024-04-20 10:45:52 +0100

    update frontend to allow upgrade insecure requests

--- next.config.mjs
+++ next.config.mjs
@@ -16,6 +16,10 @@ const nextConfig = {
             value:
               "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
           },
+          {
+            key: "Content-Security-Policy",
+            value: "upgrade-insecure-requests",
+          },
         ],
       },
     ];
