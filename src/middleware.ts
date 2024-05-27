792ff3f723a4ff6d61cef6b50a13afff51f5890e
zcemycl <lyc010197@gmail.com>
2024-05-27 02:10:47 +0100

    update

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,5 @@
 import { NextResponse, type NextRequest } from "next/server";
 import { jwtVerify, importSPKI } from "jose";
-// @ts-ignore
 import jwkToPem from "jwk-to-pem";
 
 const openid_conf_uri = process.env.NEXT_PUBLIC_COGNITO_OPENID_CONF_URI;
