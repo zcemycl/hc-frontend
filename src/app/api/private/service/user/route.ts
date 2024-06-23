acd94d071b810e2782117028c686018f8daded51
zcemycl <lyc010197@gmail.com>
2024-06-23 13:04:08 +0100

    add post user route

--- src/app/api/private/service/user/route.ts
+++ src/app/api/private/service/user/route.ts
@@ -6,6 +6,7 @@ import {
   AdminCreateUserCommand,
   AdminDeleteUserCommand,
   CognitoIdentityProviderClient,
+  CognitoIdentityProviderClientConfig,
 } from "@aws-sdk/client-cognito-identity-provider";
 
 const credentials_ = fromContainerMetadata({ timeout: 2000, maxRetries: 1 });
@@ -13,11 +14,39 @@ const credentials_ = fromContainerMetadata({ timeout: 2000, maxRetries: 1 });
 export async function POST(request: Request) {
   const data = await request.json();
   const credentials = export_aws_credentials(data, credentials_);
-  return NextResponse.json({}, { status: 200 });
+  let config = {
+    region: "eu-west-2",
+    credentials,
+  } as CognitoIdentityProviderClientConfig;
+  const client = new CognitoIdentityProviderClient(config);
+  const input = {
+    UserPoolId: "eu-west-2_wYaHLvNIY",
+    Username: data.username,
+    UserAttributes: [
+      {
+        Name: "name",
+        Value: "email",
+      },
+      {
+        Name: "email",
+        Value: data.email,
+      },
+    ],
+  };
+  const command = new AdminCreateUserCommand(input);
+  const response = await client.send(command);
+
+  return NextResponse.json(response["User"], { status: 200 });
 }
 
 export async function DELETE(request: Request) {
   const data = await request.json();
   const credentials = export_aws_credentials(data, credentials_);
+  let config = {
+    region: "eu-west-2",
+    credentials,
+  } as CognitoIdentityProviderClientConfig;
+  const client = new CognitoIdentityProviderClient(config);
+
   return NextResponse.json({}, { status: 200 });
 }
