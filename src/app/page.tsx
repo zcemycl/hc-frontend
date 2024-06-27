c753af2b72d2e21d60706caa7f4f198578848095
zcemycl <lyc010197@gmail.com>
2024-06-27 14:57:10 +0100

    update disable btn in ai tools

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -60,8 +60,8 @@ export default function Home() {
         });
         setCredentials(credentials);
         setIsAuthenticated(true);
-        // setRole(UserRoleEnum.ADMIN);
-        setRole(UserRoleEnum.USER);
+        setRole(UserRoleEnum.ADMIN);
+        // setRole(UserRoleEnum.USER);
         localStorage.setItem("credentials", credentials);
       }
     });
