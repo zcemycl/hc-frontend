70d9c257af8bc79f98bb9315363760afba194c54
zcemycl <lyc010197@gmail.com>
2024-08-26 00:09:34 +0100

    fix go icon

--- src/icons/GoIcon.tsx
+++ src/icons/GoIcon.tsx
@@ -10,9 +10,9 @@ export function GoIcon() {
       <path
         d="M13 15L16 12M16 12L13 9M16 12H8M21 12C21 16.9706 16.9706 21 12 21C7.02944 21 3 16.9706 3 12C3 7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12Z"
         stroke="#000000"
-        stroke-width="2"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
     </svg>
   );
