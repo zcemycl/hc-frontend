40ed621036406c7a40b5bf086f68015f8696810f
zcemycl <lyc010197@gmail.com>
2025-04-18 01:16:35 +0100

    changing profile page

---
+++ src/app/(auth)/profile/component.tsx
@@ -0,0 +1,119 @@
+"use client";
+import { TypographyH2, PaginationBar, ProtectedRoute } from "@/components";
+import { useAuth, useLoader } from "@/contexts";
+import { useState, useEffect } from "react";
+import { fetchUserInfoById } from "@/http/backend";
+import { useRouter } from "next/navigation";
+import { IUser } from "@/types";
+
+export default function Profile() {
+  const { userId, credentials, setIsAuthenticated, isLoadingAuth } = useAuth();
+  const { isLoading, setIsLoading } = useLoader();
+  const router = useRouter();
+  const [profileInfo, setProfileInfo] = useState<IUser | null>(null);
+
+  useEffect(() => {
+    async function getProfile(id: number) {
+      const userInfo = await fetchUserInfoById(id);
+      setProfileInfo({ ...profileInfo, ...userInfo });
+    }
+    if (isLoadingAuth) return;
+
+    if (credentials.length === 0) {
+      setIsAuthenticated(false);
+      router.push(
+        process.env.NEXT_PUBLIC_ENV_NAME !== "local-dev" ? "/logout" : "/",
+      );
+    }
+    if (!userId) return;
+    getProfile(userId as number);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [isLoadingAuth, userId]);
+
+  return (
+    <ProtectedRoute>
+      <section
+        className={`text-gray-400 bg-gray-900 body-font 
+          h-[83vh] sm:h-[90vh] overflow-y-scroll
+          ${isLoading || isLoadingAuth ? "animate-pulse" : ""}`}
+      >
+        <div
+          className="mt-[10rem] flex flex-col
+           content-center items-center
+        "
+        >
+          <div className="w-7/12 flex flex-col space-y-10">
+            <div className="flex justify-between">
+              <TypographyH2>
+                {profileInfo?.username?.toUpperCase()}
+              </TypographyH2>
+              <span className="leading-relaxed mb-1">{profileInfo?.role!}</span>
+            </div>
+            <div
+              className="
+                    grid gap-4
+                    grid-cols-1
+                    md:grid-cols-2
+                    content-center
+                    place-content-center
+                    place-items-center
+                "
+            >
+              {[
+                ["History", "Browse your search history.", "/history"],
+                [
+                  "Annotation",
+                  "View your supports in building robust AI models",
+                  "/annotations",
+                ],
+                [
+                  "Bundle",
+                  "Group your medicines of interest to compare, and let the system learn your preference.",
+                  "/bundles",
+                ],
+              ].map((v) => (
+                <div
+                  className="flex flex-col w-full
+                                    h-fit
+                                "
+                  key={`btn-group-${v[0]}`}
+                >
+                  <div className="group flex flex-col">
+                    <button
+                      className="p-4 sm:p-6
+                                        rounded-md
+                                        text-black font-bold
+                                        text-md
+                                        hover:bg-emerald-500
+                                        bg-emerald-300"
+                      onClick={() => router.push(`profile${v[2]}`)}
+                    >
+                      {v[0]}
+                    </button>
+                    <span className="max-h-0 overflow-hidden transition-all duration-300 ease-in-out group-hover:max-h-40">
+                      {v[1]}
+                    </span>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+
+        {/* <div className="container px-2 py-24 mx-auto grid justify-items-center">
+          <div className="sm:w-1/2 flex flex-col mt-8 w-screen px-10 pt-10 pb-5">
+            <div className="flex flex-col">
+              <div className="flex justify-between">
+                <TypographyH2>
+                  {profileInfo?.username?.toUpperCase()}
+                </TypographyH2>
+                <p className="leading-relaxed mb-1">{profileInfo?.role!}</p>
+              </div>
+              <p className="leading-relaxed mb-1">{profileInfo?.email!}</p>
+            </div>
+          </div>
+        </div> */}
+      </section>
+    </ProtectedRoute>
+  );
+}
---
+++ src/app/(auth)/profile/legacy.tsx
@@ -0,0 +1,217 @@
+"use client";
+import { TypographyH2, PaginationBar, ProtectedRoute } from "@/components";
+import { useAuth, useLoader } from "@/contexts";
+import { useState, useEffect } from "react";
+import {
+  fetchHistoryByUserId,
+  fetchHistoryByUserIdCount,
+  fetchUserInfoById,
+  fetchUnannotatedAETableByUserId,
+  fetchUnannotatedAETableByUserIdCount,
+} from "@/http/backend";
+import { useRouter } from "next/navigation";
+import {
+  IUser,
+  SearchActionEnum,
+  UserHistoryCategoryEnum,
+  AnnotationCategoryEnum,
+  IHistory,
+  IUnAnnotatedAETable,
+} from "@/types";
+import { convert_datetime_to_simple } from "@/utils";
+
+export default function Profile() {
+  const { userId, credentials, setIsAuthenticated, isLoadingAuth } = useAuth();
+  const { isLoading, setIsLoading } = useLoader();
+  const router = useRouter();
+  const [profileInfo, setProfileInfo] = useState<IUser | null>(null);
+  const [history, setHistory] = useState<IHistory[]>([]);
+  const [tableData, setTableData] = useState<IUnAnnotatedAETable[]>([]);
+  const [countAnnotated, setCountAnnotated] = useState(0);
+  const [countHistory, setCountHistory] = useState(0);
+  const [nPerPage, _] = useState(10);
+  const [pageNHistory, setPageNHistory] = useState(0);
+  const [pageNAnnotate, setPageNAnnotate] = useState(0);
+
+  async function setHistoryData(id: number) {
+    const historyInfo = await fetchHistoryByUserId(
+      id,
+      nPerPage * pageNHistory,
+      nPerPage,
+    );
+    setHistory(historyInfo);
+  }
+
+  async function setAnnotationRecord(id: number) {
+    const annotatedData = await fetchUnannotatedAETableByUserId(
+      id,
+      AnnotationCategoryEnum.ADVERSE_EFFECT_TABLE,
+      nPerPage * pageNAnnotate,
+      nPerPage,
+      true,
+    );
+    if (annotatedData !== undefined) setTableData(annotatedData);
+  }
+
+  useEffect(() => {
+    if (isLoadingAuth) return;
+    if (!userId) return;
+    setHistoryData(userId as number);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [isLoadingAuth, userId, pageNHistory]);
+
+  useEffect(() => {
+    if (isLoadingAuth) return;
+    if (!userId) return;
+    setAnnotationRecord(userId as number);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [isLoadingAuth, userId, pageNAnnotate]);
+
+  useEffect(() => {
+    async function getProfile(id: number) {
+      const userInfo = await fetchUserInfoById(id);
+      setProfileInfo({ ...profileInfo, ...userInfo });
+      const historyCount = await fetchHistoryByUserIdCount(id);
+      setCountHistory(historyCount);
+      const numberAnnotated = await fetchUnannotatedAETableByUserIdCount(
+        id,
+        AnnotationCategoryEnum.ADVERSE_EFFECT_TABLE,
+        true,
+      );
+      setCountAnnotated(numberAnnotated);
+    }
+    if (isLoadingAuth) return;
+
+    if (credentials.length === 0) {
+      setIsAuthenticated(false);
+      router.push(
+        process.env.NEXT_PUBLIC_ENV_NAME !== "local-dev" ? "/logout" : "/",
+      );
+    }
+    if (!userId) return;
+    getProfile(userId as number);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [isLoadingAuth, userId]);
+
+  return (
+    <ProtectedRoute>
+      <section
+        className={`text-gray-400 bg-gray-900 body-font 
+          h-[83vh] sm:h-[90vh] overflow-y-scroll
+          ${isLoading || isLoadingAuth ? "animate-pulse" : ""}`}
+      >
+        <div className="container px-2 py-24 mx-auto grid justify-items-center">
+          <div className="sm:w-1/2 flex flex-col mt-8 w-screen px-10 pt-10 pb-5">
+            <div className="flex flex-col">
+              <div className="flex justify-between">
+                <TypographyH2>
+                  {profileInfo?.username?.toUpperCase()}
+                  {" 's"} History
+                </TypographyH2>
+                <p className="leading-relaxed mb-1">{profileInfo?.role!}</p>
+              </div>
+              <p className="leading-relaxed mb-1">{profileInfo?.email!}</p>
+
+              <hr className="mb-2" />
+              <TypographyH2>Search Activities X{countHistory}</TypographyH2>
+              {!!history && history.length === 0 ? (
+                <p className="leading-relaxed mb-1">No Record ...</p>
+              ) : (
+                history
+                  ?.filter((x) => x.category === UserHistoryCategoryEnum.SEARCH)
+                  .map((x, idx) => {
+                    return (
+                      <button
+                        disabled={
+                          x.detail.action === SearchActionEnum.COMPARE_AE
+                        }
+                        key={`history-${idx}`}
+                        onClick={(e) => {
+                          router.push(`/search?historyId=${x.id}`);
+                        }}
+                        className="hover:bg-green-200 
+                        text-clip overflow-clip
+                        focus:bg-blue-200 transition
+                        justify-start content-start
+                        rounded px-2 hover:text-black"
+                      >
+                        <div className="flex justify-between">
+                          <p className="leading-relaxed">
+                            {convert_datetime_to_simple(x.created_date)}
+                          </p>
+                          <p className="leading-relaxed">{x.detail.action}</p>
+                          <p className="leading-relaxed">
+                            {x.detail.additional_settings.queryType}
+                          </p>
+                        </div>
+                        <p
+                          className="text-xs text-left text-clip
+                          whitespace-nowrap"
+                        >
+                          {`[\"${x.detail.query.join('", "')}\"]`}
+                        </p>
+                      </button>
+                    );
+                  })
+              )}
+              <div className="flex justify-center space-x-1 flex-wrap">
+                <PaginationBar
+                  topN={countHistory}
+                  pageN={pageNHistory}
+                  nPerPage={nPerPage}
+                  setPageN={(i: number) => {
+                    setPageNHistory(i);
+                  }}
+                />
+              </div>
+              <hr className="mb-2" />
+              <TypographyH2>Annotation X{countAnnotated}</TypographyH2>
+              {tableData.length === 0 ? (
+                <p className="leading-relaxed mb-1">No Record ...</p>
+              ) : (
+                tableData.map((x, idx) => {
+                  return (
+                    <button
+                      key={`history-annotation-${idx}`}
+                      className="hover:bg-green-200 
+                        text-clip overflow-clip
+                        focus:bg-blue-200 transition
+                        justify-start content-start
+                        rounded px-2 hover:text-black"
+                      onClick={(e) => {
+                        e.preventDefault();
+                        router.push(
+                          `/annotate/fdalabel/${x.fdalabel.setid}/adverse_effect_table/${x.idx}`,
+                        );
+                      }}
+                    >
+                      <div className="flex justify-between">
+                        <p className="leading-relaxed">
+                          {convert_datetime_to_simple(x.created_date as string)}
+                        </p>
+                        <p className="leading-relaxed">
+                          {x.fdalabel.tradename}
+                        </p>
+                        <p className="leading-relaxed">Table {x.idx}</p>
+                      </div>
+                    </button>
+                  );
+                })
+              )}
+              <div className="flex justify-center space-x-1 flex-wrap">
+                <PaginationBar
+                  topN={countAnnotated}
+                  pageN={pageNAnnotate}
+                  nPerPage={nPerPage}
+                  setPageN={(i: number) => {
+                    setPageNAnnotate(i);
+                  }}
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </ProtectedRoute>
+  );
+}
