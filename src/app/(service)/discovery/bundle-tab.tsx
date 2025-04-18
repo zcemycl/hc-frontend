ca0bd660b9e07ac06ce62b6c088d1ac14ecadbb2
zcemycl <lyc010197@gmail.com>
2025-04-18 20:21:09 +0100

    init bundle tab for discovery page

--- src/app/(service)/discovery/bundle-tab.tsx
+++ src/app/(service)/discovery/bundle-tab.tsx
@@ -9,19 +9,19 @@ export default function BundleTab() {
   return (
     <div
       className={`absolute
-                       left-0 right-0 top-0 bottom-0
-                       self-end
-                       space-y-2
-                       overflow-y-auto
-                       w-full h-full
-                       transition
-                       p-5
-                       ${
-                         tab === GraphTabEnum.bundle
-                           ? "opacity-100 z-10 delay-200"
-                           : "opacity-0 z-0 duration-200"
-                       }
-                       `}
+        left-0 right-0 top-0 bottom-0
+        self-end
+        space-y-2
+        overflow-y-auto
+        w-full h-full
+        transition
+        p-5
+        ${
+          tab === GraphTabEnum.bundle
+            ? "opacity-100 z-10 delay-200"
+            : "opacity-0 z-0 duration-200"
+        }
+        `}
     >
       <h2 className="leading text-slate-300 font-bold">Bundles</h2>
     </div>
