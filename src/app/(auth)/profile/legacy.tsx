1c707fb2c05bc4891d83c8481306b06e970f0186
zcemycl <lyc010197@gmail.com>
2025-04-18 09:40:20 +0100

    removing history from legacy

--- src/app/(auth)/profile/legacy.tsx
+++ src/app/(auth)/profile/legacy.tsx
@@ -3,21 +3,12 @@ import { TypographyH2, PaginationBar, ProtectedRoute } from "@/components";
 import { useAuth, useLoader } from "@/contexts";
 import { useState, useEffect } from "react";
 import {
-  fetchHistoryByUserId,
-  fetchHistoryByUserIdCount,
   fetchUserInfoById,
   fetchUnannotatedAETableByUserId,
   fetchUnannotatedAETableByUserIdCount,
 } from "@/http/backend";
 import { useRouter } from "next/navigation";
-import {
-  IUser,
-  SearchActionEnum,
-  UserHistoryCategoryEnum,
-  AnnotationCategoryEnum,
-  IHistory,
-  IUnAnnotatedAETable,
-} from "@/types";
+import { IUser, AnnotationCategoryEnum, IUnAnnotatedAETable } from "@/types";
 import { convert_datetime_to_simple } from "@/utils";
 
 export default function Profile() {
@@ -25,22 +16,10 @@ export default function Profile() {
   const { isLoading, setIsLoading } = useLoader();
   const router = useRouter();
   const [profileInfo, setProfileInfo] = useState<IUser | null>(null);
-  const [history, setHistory] = useState<IHistory[]>([]);
   const [tableData, setTableData] = useState<IUnAnnotatedAETable[]>([]);
   const [countAnnotated, setCountAnnotated] = useState(0);
-  const [countHistory, setCountHistory] = useState(0);
   const [nPerPage, _] = useState(10);
-  const [pageNHistory, setPageNHistory] = useState(0);
   const [pageNAnnotate, setPageNAnnotate] = useState(0);
-
-  async function setHistoryData(id: number) {
-    const historyInfo = await fetchHistoryByUserId(
-      id,
-      nPerPage * pageNHistory,
-      nPerPage,
-    );
-    setHistory(historyInfo);
-  }
 
   async function setAnnotationRecord(id: number) {
     const annotatedData = await fetchUnannotatedAETableByUserId(
@@ -56,13 +35,6 @@ export default function Profile() {
   useEffect(() => {
     if (isLoadingAuth) return;
     if (!userId) return;
-    setHistoryData(userId as number);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [isLoadingAuth, userId, pageNHistory]);
-
-  useEffect(() => {
-    if (isLoadingAuth) return;
-    if (!userId) return;
     setAnnotationRecord(userId as number);
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [isLoadingAuth, userId, pageNAnnotate]);
@@ -71,8 +43,6 @@ export default function Profile() {
     async function getProfile(id: number) {
       const userInfo = await fetchUserInfoById(id);
       setProfileInfo({ ...profileInfo, ...userInfo });
-      const historyCount = await fetchHistoryByUserIdCount(id);
-      setCountHistory(historyCount);
       const numberAnnotated = await fetchUnannotatedAETableByUserIdCount(
         id,
         AnnotationCategoryEnum.ADVERSE_EFFECT_TABLE,
@@ -111,59 +81,6 @@ export default function Profile() {
                 <p className="leading-relaxed mb-1">{profileInfo?.role!}</p>
               </div>
               <p className="leading-relaxed mb-1">{profileInfo?.email!}</p>
-
-              <hr className="mb-2" />
-              <TypographyH2>Search Activities X{countHistory}</TypographyH2>
-              {!!history && history.length === 0 ? (
-                <p className="leading-relaxed mb-1">No Record ...</p>
-              ) : (
-                history
-                  ?.filter((x) => x.category === UserHistoryCategoryEnum.SEARCH)
-                  .map((x, idx) => {
-                    return (
-                      <button
-                        disabled={
-                          x.detail.action === SearchActionEnum.COMPARE_AE
-                        }
-                        key={`history-${idx}`}
-                        onClick={(e) => {
-                          router.push(`/search?historyId=${x.id}`);
-                        }}
-                        className="hover:bg-green-200 
-                        text-clip overflow-clip
-                        focus:bg-blue-200 transition
-                        justify-start content-start
-                        rounded px-2 hover:text-black"
-                      >
-                        <div className="flex justify-between">
-                          <p className="leading-relaxed">
-                            {convert_datetime_to_simple(x.created_date)}
-                          </p>
-                          <p className="leading-relaxed">{x.detail.action}</p>
-                          <p className="leading-relaxed">
-                            {x.detail.additional_settings.queryType}
-                          </p>
-                        </div>
-                        <p
-                          className="text-xs text-left text-clip
-                          whitespace-nowrap"
-                        >
-                          {`[\"${x.detail.query.join('", "')}\"]`}
-                        </p>
-                      </button>
-                    );
-                  })
-              )}
-              <div className="flex justify-center space-x-1 flex-wrap">
-                <PaginationBar
-                  topN={countHistory}
-                  pageN={pageNHistory}
-                  nPerPage={nPerPage}
-                  setPageN={(i: number) => {
-                    setPageNHistory(i);
-                  }}
-                />
-              </div>
               <hr className="mb-2" />
               <TypographyH2>Annotation X{countAnnotated}</TypographyH2>
               {tableData.length === 0 ? (
