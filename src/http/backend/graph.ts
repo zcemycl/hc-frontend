337523f52dfb58a29ecaad0196457742bb73995f
Leo Leung <42243073+zcemycl@users.noreply.github.com>
2025-08-17 20:13:50 +0100

    fix: graph db version support (#29)
    * fixing post graph endpoint
    
    * updating discovery graph logic with product name or not
    
    * updating discovery graph logic with product name or not

--- src/http/backend/graph.ts
+++ src/http/backend/graph.ts
@@ -1,4 +1,5 @@
 "use server";
+import { cookies } from "next/headers";
 import { get_token_cookie } from "../utils-server";
 import { FASTAPI_URI } from "./constants";
 
@@ -6,21 +7,28 @@ export async function fetchGraphDummy(
   name: string = "Neoplasms",
   limit: number = 50,
   offset: number = 0,
+  product: string | null = null,
 ) {
+  const cookie = cookies();
+  const versions = JSON.parse(cookie.get("fda-scrape-cur-version")!.value);
   const token = await get_token_cookie();
   const API_URI = `${FASTAPI_URI}/graph`;
   const params = new URLSearchParams();
   params.append("name", name);
   params.append("limit", limit.toString());
   params.append("offset", offset.toString());
+  if (product != null) {
+    params.append("product", product);
+  }
   const API_URI_COMPLETE = `${API_URI}/?${params}`;
 
   const response = await fetch(API_URI_COMPLETE, {
-    method: "GET",
+    method: "POST",
     headers: {
       "Content-Type": "application/json",
       Authorization: `Bearer ${token}`,
     },
+    body: JSON.stringify(versions),
   });
   const res = await response.json();
   return res;
