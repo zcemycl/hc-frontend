0839af3f2350cb8584f86b1fb50e992fb5443108
zcemycl <lyc010197@gmail.com>
2025-08-20 17:20:21 +0100

    fixing

--- src/app/sample/page.tsx
+++ src/app/sample/page.tsx
@@ -1,15 +1,15 @@
 "use server";
-import { fetchFdalabelCount } from "@/http/backend";
+import { fetchFdalabelCountv2 } from "@/http/backend";
 import Component from "./component";
 import { SampleProvider } from "./context";
 import { cookies } from "next/headers";
 
 export default async function SamplePage() {
   const tabName = cookies().get("sample/tabName")?.value ?? "unknown";
-  const fdalabelCount = await fetchFdalabelCount();
+  const fdalabelCount = await fetchFdalabelCountv2();
 
   return (
-    <SampleProvider tabName={tabName} count={fdalabelCount}>
+    <SampleProvider tabName={tabName} count={fdalabelCount.data ?? 0}>
       <Component />
     </SampleProvider>
   );
