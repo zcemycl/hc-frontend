d59256479bde4f2ccd3856b8f03104ba4c76862f
zcemycl <lyc010197@gmail.com>
2025-09-12 01:37:34 +0100

    update bundles profile

--- src/app/(auth)/profile/bundles/page.tsx
+++ src/app/(auth)/profile/bundles/page.tsx
@@ -1,26 +1,43 @@
 "use client";
-import { ProtectedRoute } from "@/components";
+import { ProtectedRoute, TypographyH2 } from "@/components";
 import { useAuth, useLoader } from "@/contexts";
 import ProfileBar from "../profile-bar";
 import { useEffect, useState } from "react";
-import { fetchUserInfoByIdv2 } from "@/http/backend";
-import { IUser } from "@/types";
+import {
+  fetchBundlesByUserIdv2,
+  fetchBundlesCountByUserIdv2,
+  fetchUserInfoByIdv2,
+} from "@/http/backend";
+import { IBundle, IUser } from "@/types";
 import { useApiHandler } from "@/hooks";
 
 export default function Page() {
   const { userId, isLoadingAuth } = useAuth();
-  const { isLoadingv2 } = useLoader();
+  const { isLoadingv2, withLoading } = useLoader();
   const { handleResponse } = useApiHandler();
   const [profileInfo, setProfileInfo] = useState<IUser | null>(null);
+  const [bundles, setBundles] = useState<IBundle[]>([]);
+  const [bundlesCount, setBundlesCount] = useState(0);
+  const nPerPage = 10;
+  const [pageN, setPageN] = useState(0);
 
   useEffect(() => {
     async function getProfile(id: number) {
       const userInfo = await fetchUserInfoByIdv2(id);
       handleResponse(userInfo);
       if (userInfo.success) setProfileInfo(userInfo.data ?? null);
+      const [tmpBundlesRes, tmpBundlesCount] = await withLoading(() =>
+        Promise.all([
+          fetchBundlesByUserIdv2(userId as number, nPerPage * pageN, nPerPage),
+          fetchBundlesCountByUserIdv2(userId as number),
+        ]),
+      );
+      handleResponse(tmpBundlesRes);
+      setBundles(tmpBundlesRes.data ?? []);
+      handleResponse(tmpBundlesCount);
+      setBundlesCount(tmpBundlesCount.data ?? 0);
     }
     if (isLoadingAuth) return;
-    if (!userId) return;
     getProfile(userId as number);
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [isLoadingAuth, userId]);
@@ -43,6 +60,15 @@ export default function Page() {
                 role: profileInfo?.role!,
               }}
             />
+            <hr className="mb-2" />
+            <TypographyH2>Bundles X{bundlesCount}</TypographyH2>
+            <div className="flex flex-col space-y-1">
+              {bundles.length === 0 ? (
+                <p className="leading-relaxed mb-1">No Record ...</p>
+              ) : (
+                <></>
+              )}
+            </div>
           </div>
         </div>
       </section>
