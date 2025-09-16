b8caad708632b8d693d3759d73537d673640e73a
zcemycl <lyc010197@gmail.com>
2025-09-17 00:54:59 +0100

    refactoring

--- src/app/(auth)/profile/component.tsx
+++ src/app/(auth)/profile/component.tsx
@@ -1,6 +1,11 @@
 "use client";
-import { ProtectedRoute, PulseTemplate } from "@/components";
-import { useAuth } from "@/contexts";
+import {
+  ListPageTemplate,
+  ProtectedRoute,
+  PulseTemplate,
+  Spinner,
+} from "@/components";
+import { useAuth, useLoader } from "@/contexts";
 import { useState, useEffect } from "react";
 import { fetchUserInfoByIdv2 } from "@/http/backend";
 import { useRouter } from "next/navigation";
@@ -11,6 +16,7 @@ import { useApiHandler } from "@/hooks";
 export default function Profile() {
   const { handleResponse } = useApiHandler();
   const { userId, isLoadingAuth } = useAuth();
+  const { isLoadingv2 } = useLoader();
   const router = useRouter();
   const [profileInfo, setProfileInfo] = useState<IUser | null>(null);
 
@@ -28,24 +34,22 @@ export default function Profile() {
 
   return (
     <ProtectedRoute>
-      <PulseTemplate overflowY={true}>
-        <div
-          className="mt-[10rem] flex flex-col
-           content-center items-center
-        "
-        >
-          <div className="w-11/12 sm:w-7/12 flex flex-col space-y-10">
-            <ProfileBar
-              {...{
-                username: profileInfo?.username! as string,
-                role: profileInfo?.role!,
-              }}
-            >
-              <p className="leading-relaxed mb-1">{profileInfo?.email!}</p>
-            </ProfileBar>
+      <ListPageTemplate>
+        {isLoadingv2 ? (
+          <Spinner />
+        ) : (
+          <ProfileBar
+            {...{
+              username: profileInfo?.username! as string,
+              role: profileInfo?.role!,
+            }}
+          >
+            <p className="leading-relaxed mb-1">{profileInfo?.email!}</p>
+          </ProfileBar>
+        )}
 
-            <div
-              className="
+        <div
+          className="
                     grid gap-4
                     grid-cols-1
                     md:grid-cols-2
@@ -54,52 +58,50 @@ export default function Profile() {
                     place-content-center
                     place-items-center
                 "
-            >
-              {[
-                ["History", "Browse your search history.", "/history"],
-                [
-                  "Annotation",
-                  "View your supports in building robust AI models",
-                  "/annotations",
-                ],
-                [
-                  "Bundle",
-                  "Group your medicines of interest to compare, and let the system learn your preference.",
-                  "/bundles",
-                ],
-              ].map((v) => (
-                <div
-                  className="flex flex-col w-full
+        >
+          {[
+            ["History", "Browse your search history.", "/history"],
+            [
+              "Annotation",
+              "View your supports in building robust AI models",
+              "/annotations",
+            ],
+            [
+              "Bundle",
+              "Group your medicines of interest to compare, and let the system learn your preference.",
+              "/bundles",
+            ],
+          ].map((v) => (
+            <div
+              className="flex flex-col w-full
                                     h-fit
                                 "
-                  key={`btn-group-${v[0]}`}
-                >
-                  <div className="group flex flex-col">
-                    <button
-                      className="p-4 sm:p-6
+              key={`btn-group-${v[0]}`}
+            >
+              <div className="group flex flex-col">
+                <button
+                  className="p-4 sm:p-6
                         rounded-md
                         text-black font-bold
                         text-md
                         hover:bg-emerald-500
                         bg-emerald-300"
-                      onClick={() => router.push(`profile${v[2]}`)}
-                    >
-                      {v[0]}
-                    </button>
-                    <span
-                      className="max-h-0 overflow-hidden 
+                  onClick={() => router.push(`profile${v[2]}`)}
+                >
+                  {v[0]}
+                </button>
+                <span
+                  className="max-h-0 overflow-hidden 
                         transition-all duration-300 
                         ease-in-out group-hover:max-h-40"
-                    >
-                      {v[1]}
-                    </span>
-                  </div>
-                </div>
-              ))}
+                >
+                  {v[1]}
+                </span>
+              </div>
             </div>
-          </div>
+          ))}
         </div>
-      </PulseTemplate>
+      </ListPageTemplate>
     </ProtectedRoute>
   );
 }
