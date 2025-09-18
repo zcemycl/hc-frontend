63f92f90b372e00e66f41e486a216dcd983ad1aa
zcemycl <lyc010197@gmail.com>
2025-09-18 10:13:32 +0100

    update

--- src/app/(service)/annotate/bundle/page.tsx
+++ src/app/(service)/annotate/bundle/page.tsx
@@ -3,9 +3,12 @@ import { useSearchParams } from "next/navigation";
 import { PageProps } from "./props";
 import { useEffect } from "react";
 import { ListPageTemplate, ProfileBar, ProtectedRoute } from "@/components";
+import { useLoader } from "@/contexts";
 
 export default function Page({ params }: Readonly<PageProps>) {
   const searchParams = useSearchParams();
+  const { isLoadingv2, withLoading } = useLoader();
+
   useEffect(() => {
     console.log(searchParams);
   }, []);
