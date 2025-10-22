ed14063e75411f518c76a3008ff6097168b5b366
zcemycl <lyc010197@gmail.com>
2025-10-22 23:20:40 +0100

    add owner btn to bundle page

--- src/app/(service)/bundle/page.tsx
+++ src/app/(service)/bundle/page.tsx
@@ -1,9 +1,15 @@
 "use client";
-import { ListPageTemplate, ProfileBar, ProtectedRoute } from "@/components";
+import {
+  ListPageTemplate,
+  Modal,
+  ProfileBar,
+  ProtectedRoute,
+} from "@/components";
 import { useLoader } from "@/contexts";
 import { useApiHandler } from "@/hooks";
 import { fetchBundleByIdv2 } from "@/http/backend";
 import { IAnnotationRef, IBundle, IFdaLabelRef, IUserRef } from "@/types";
+import { Plus, SendHorizontal } from "lucide-react";
 import { useSearchParams } from "next/navigation";
 import { useEffect, useMemo, useState } from "react";
 
@@ -13,6 +19,8 @@ export default function Page() {
   const [bundle, setBundle] = useState<IBundle | null>(null);
   const { withLoading } = useLoader();
   const { handleResponse } = useApiHandler();
+  const [isOpenModal, setIsOpenModal] = useState(false);
+  const [email, setEmail] = useState("");
 
   useEffect(() => {
     const getData = async () => {
@@ -76,7 +84,7 @@ export default function Page() {
           {bundle?.users.length !== 0 && (
             <div
               className="flex flex-row justify-start space-x-2
-                flex-wrap"
+                flex-wrap items-center"
             >
               <span>Owners: </span>
               {bundle?.users.map((u: IUserRef) => (
@@ -88,6 +96,20 @@ export default function Page() {
                   {u.email}
                 </span>
               ))}
+              <button
+                onClick={(e) => {
+                  e.preventDefault();
+                  setIsOpenModal(true);
+                }}
+              >
+                <Plus
+                  className="
+                  rounded-lg bg-sky-400
+                  hover:bg-sky-700
+                  text-black
+                "
+                />
+              </button>
             </div>
           )}
           {bundle?.fdalabels.length !== 0 && (
@@ -125,6 +147,49 @@ export default function Page() {
             </div>
           )}
         </div>
+        <Modal
+          {...{
+            title: "Add user by email",
+            isOpenModal,
+            setIsOpenModal,
+          }}
+        >
+          <div
+            className="flex flex-col space-y-2
+                  px-2 sm:px-5
+                  py-2 pb-5"
+          >
+            <div className="flex flex-col space-y-1">
+              <label className="font-bold text-white">Email Address</label>
+              <input
+                type="text"
+                value={email}
+                className="bg-slate-300 text-black w-full
+                          rounded-md p-2"
+                onChange={(e) => {
+                  e.preventDefault();
+                  setEmail(e.target.value);
+                }}
+              />
+              <div className="flex flex-row justify-end">
+                <button
+                  className="bg-emerald-300 hover:bg-emerald-500
+                          rounded-lg flex flex-row justify-between
+                          px-4 py-2 space-x-2
+                          font-bold text-black"
+                  onClick={async (e) => {
+                    e.preventDefault();
+                    setEmail("");
+                    setIsOpenModal(false);
+                  }}
+                >
+                  <SendHorizontal />
+                  <span>Submit</span>
+                </button>
+              </div>
+            </div>
+          </div>
+        </Modal>
       </ListPageTemplate>
     </ProtectedRoute>
   );
