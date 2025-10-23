010bc047b63e61c95bba99710a4db1c5e5c41c7e
zcemycl <lyc010197@gmail.com>
2025-10-23 01:10:21 +0100

    refactoring

--- src/app/(service)/discovery/tabs/bundle/components/move-del-btn.tsx
+++ src/app/(service)/discovery/tabs/bundle/components/move-del-btn.tsx
@@ -16,36 +16,41 @@ export const MoveDelBtn = ({
   return (
     <div
       className={`flex items-center rounded-lg
-            truncate overflow-x-auto relative
+            overflow-visible
             ${switch_color_node(node.group as string)}
             ${switch_hover_color_node(node.group as string)}
             cursor-pointer
             h-8 px-3 py-1`}
-      onClick={(e) => {
-        e.preventDefault();
-        click_callback(node.id);
-      }}
     >
       <div
-        className={`text-black font-medium whitespace-nowrap
-            transition-transform duration-1000`}
-      >
-        {node.label}
-      </div>
-      <button
-        className="flex items-center justify-center
-                absolute right-0 top-0
-                rounded-full bg-red-400 
-                hover:bg-red-500 transition-colors"
-        aria-label={`Remove ${node.label}`}
+        className="relative w-full"
         onClick={(e) => {
           e.preventDefault();
-          e.stopPropagation();
-          del_callback(node.id, idx);
+          click_callback(node.id);
         }}
       >
-        <img src={X_CIRCLE_ICON_URI} />
-      </button>
+        <div
+          className={`text-black font-medium whitespace-nowrap
+            transition-transform duration-1000`}
+        >
+          {node.label}
+        </div>
+        <button
+          className="flex items-center justify-center
+                absolute -top-2 -right-2
+                rounded-full bg-red-400
+                translate-x-2 z-10
+                hover:bg-red-500 transition-colors"
+          aria-label={`Remove ${node.label}`}
+          onClick={(e) => {
+            e.preventDefault();
+            e.stopPropagation();
+            del_callback(node.id, idx);
+          }}
+        >
+          <img src={X_CIRCLE_ICON_URI} />
+        </button>
+      </div>
     </div>
   );
 };
