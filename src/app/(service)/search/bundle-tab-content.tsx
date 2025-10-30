bd27c98bc80bf08d8c7ac91d8e70adabcd991794
zcemycl <lyc010197@gmail.com>
2025-10-30 12:17:15 +0000

    refactoring

---
+++ src/app/(service)/search/bundle-tab-content.tsx
@@ -0,0 +1,11 @@
+import { BundleConnectEnum } from "@/constants";
+
+const BundleTabContent = ({
+  mode = null,
+}: {
+  mode: BundleConnectEnum | null;
+}) => {
+  return <></>;
+};
+
+export { BundleTabContent };
